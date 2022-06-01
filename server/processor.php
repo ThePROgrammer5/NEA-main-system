372c3a928d7972de8cc976bf8694c08ce19b76c9
The PROgrammer / <codingplusgaming@gmail.com>
2022-06-01 11:25:07 -0700

    done

--- server/processor.php
+++ server/processor.php
@@ -3,4 +3,11 @@
 $name = $_POST['firstName'];
 file_put_contents('name.php', $name, FILE_APPEND);
 
+
+$mName = $_POST['middleName'];
+file_put_contents('mName.php', $mName, FILE_APPEND);
+
+
+
+
 ?>
