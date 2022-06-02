c9f29aefbf997858677caf652c2d808243437a8c
The PROgrammer / <codingplusgaming@gmail.com>
2022-06-02 08:57:09 -0700

    done

--- processor.php
+++ processor.php
@@ -2,16 +2,16 @@
 <body>
 <?php
 
-if (isset( $_POST['submit'])) {
-$name = $_POST['firstName'];
+if (isset( $_GET['submit'])) {
+$name = $_GET['firstName'];
 file_put_contents('name.php', $name, FILE_APPEND);
 
 
-$mName = $_POST['middleName'];
+$mName = $_GET['middleName'];
 file_put_contents('mName.php', $mName, FILE_APPEND);
 
 
-$lName = $_POST['lastName'];
+$lName = $_GET['lastName'];
 file_put_contents('lName.php', $lName, FILE_APPEND);
 }
 
